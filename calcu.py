05aad456e59a8f01a765289650a542a36c5623a1
Muambeli365 <muambeli.sinthumule@enviro365.co.za>
2026-09-17 21:07:53 +0200

    initial commit

---
+++ calcu.py
@@ -0,0 +1,5 @@
+first = float(input("Enter the first number: "))
+second = float(input("Enter the second number: "))
+third = float(input("Enter the third number: "))
+
+print("Total:", first + second + third)
