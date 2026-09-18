1a0322b99c0d211968d0180d673475ee7f429b2c
Muambeli365 <muambeli.sinthumule@enviro365.co.za>
2026-09-18 08:26:26 +0200

    intial commit

--- calcu.py
+++ calcu.py
@@ -2,5 +2,7 @@ first = float(input("Enter the first number: "))
 second = float(input("Enter the second number: "))
 third = float(input("Enter the third number: "))
 fourth = float(input("Enter the fourth number: "))
+fifth = float(input("Enter the fifth number: "))
+sixth = float(input("Enter the sixth number: "))
 
-print("Total:", first + second + third + fourth)
+print("Total:", first + second + third + fourth + fifth + sixth)
